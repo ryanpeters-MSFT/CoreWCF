2c7d43796a2c67aabdad8c7d9f856da7516cc3b9
Ryan Peters <ryanpeters@microsoft.com>
2023-04-24 20:29:40 -0400

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using ServiceReference1;
+
+using (var client = new ServiceClient())
+{
+    var compositeType = new CompositeType
+    {
+        BoolValue = true,
+        StringValue = "Hello"
+    };
+
+    var response = await client.GetDataUsingDataContractAsync(compositeType);
+
+    Console.WriteLine(response.StringValue);
+}
